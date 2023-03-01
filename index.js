8a6da4baadbaeb6dca05a3b666c6dc5ed309f74f
sranik28~ <anikshariarrahman@gmail.com>
2023-03-01 20:28:15 +0600

    shoew more btn

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const showData = async (phnName) => {
-  const url = `https://openapi.programming-hero.com/api/phones?search=${phnName}`
+let count = 9;
+const phonPraint = document.getElementById('phon-Details');
+const showMoreBtn = document.getElementById('show-more-btn');
+const phoneDetails = async () => {
+  const inputInfo = document.getElementById('input-info');
+  const url = `https://openapi.programming-hero.com/api/phones?search=${inputInfo.value}`
   let res = await fetch(url),
-    data = await res.json()
-  phoneDetails(data.data)
-}
-const phonPraint = document.getElementById('phon-Details')
-const phoneDetails = (phones) => {
+    data = await res.json();
 
+    console.log(data)
   // show more baki
-
-  const showMoreBtn = document.getElementById('show-more-btn')
+  let phones = data.data
   if (phones.length > 10) {
-    phones = phones.slice(0, 10)
+    phones = phones.slice(0, count)
     showMoreBtn.classList.remove("hidden")
   }
   else {
     showMoreBtn.classList.add("hidden")
   }
-  phones = phones.slice(0, 6)
+  // phones = phones.slice(0, 6)
   const noFoundMassege = document.getElementById('massege');
   if (phones.length === 0) {
     noFoundMassege.classList.remove('hidden');
@@ -25,10 +25,14 @@ const phoneDetails = (phones) => {
   else {
     noFoundMassege.classList.add('hidden');
   }
+  if(phones.length < count){
+    count = phones.length;
+    showMoreBtn.classList.add("hidden")
+  }
   phonPraint.innerHTML = "";
 
   phones.forEach(phone => {
-    // console.log(phone)
+    console.log(phone)
     const phoneDiv = document.createElement('div');
     phoneDiv.className = "card card-compact w-96 bg-base-100 shadow-2xl my-16"
     phoneDiv.innerHTML = `
@@ -38,13 +42,11 @@ const phoneDetails = (phones) => {
                    <p>${phone.phone_name}</p>                  
                    <div class="card-actions justify-end">
                      <button class="btn btn-primary">Buy Now</button>
-                   </div>
-                  
-                     <label onclick ="loadDitails('${phone[0].slug}')" for="my-modal">Details</label>
+                   </div>                 
+                     <label onclick ="loadDitails('${phone.slug}')" for="my-modal">Details</label>
               </div> 
         `
     phonPraint.appendChild(phoneDiv)
-    console.log(phones)
   });
   laoding(false)
 }
@@ -52,11 +54,10 @@ const phoneDetails = (phones) => {
 // spiner
 
 const searchBtn = document.getElementById('search-btn');
+
 searchBtn.onclick = () => {
   laoding(true)
-  const inputInfo = document.getElementById('input-info');
-  showData(inputInfo.value)
-  inputInfo.value = '';
+  phoneDetails()
 }
 const laoding = (isLadiong) => {
   const loadingSpine = document.getElementById('loading-spiner');
@@ -87,7 +88,11 @@ const phoneDitails = (phones) => {
   `
   console.log(phones)
 }
-
+// show more btn
+showMoreBtn.onclick = () =>{
+  count += 3;
+  phoneDetails();
+}
 
  
 
